7ddd2cd6bf9187203ccd63ec88ccfee124045ef3
Jonti Hudson <33636074+JontiHudson@users.noreply.github.com>
2022-01-13 09:07:18 +0000

    1.6.0-beta46

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createNotificationManager = void 0;
-const NotificationStateClass_1 = require("./NotificationStateClass");
-function createNotificationManager(options) {
-    return new NotificationStateClass_1.NotificationStateClass(options);
-}
-exports.createNotificationManager = createNotificationManager;
